dd6d5f8dacfefe9b5d9aaacad7096bf374b89c02
Franco Contreras <franco.contreras20@inacapmail.cl>
2026-07-05 11:09:10 -0400

    feat: implementación de estado global con useContext y useReducer para sesión

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// Importamos nuestro Proveedor
+import { AuthProvider } from './context/AuthContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* Envolvemos la App para que tenga acceso global a la sesión */}
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
 )
